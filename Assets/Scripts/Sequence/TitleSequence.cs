43b6224a32176559d609e3b45b6a99161bb800ae
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-07 23:20:33 +0900

    接続～入室までの流れを実装

--- Assets/Scripts/Sequence/TitleSequence.cs
+++ Assets/Scripts/Sequence/TitleSequence.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UniRx;
+using Cysharp.Threading.Tasks;
+using Pun2Task;
+using System;
+using Photon.Realtime;
 
 namespace Sequence
 {
@@ -15,5 +20,27 @@ namespace Sequence
         /// </summary>
         [SerializeField]
         private Button startButton = null;
+
+        void Awake()
+        {
+            startButton.OnClickAsObservable()
+                       .Subscribe(async _ =>
+                       {
+                           startButton.interactable = false;
+                           var token = this.GetCancellationTokenOnDestroy();
+                           try
+                           {
+                               await Pun2TaskNetwork.ConnectUsingSettingsAsync(token);
+                               Debug.Log("Connect OK");
+                               await Pun2TaskNetwork.JoinOrCreateRoomAsync("TestRoom", new RoomOptions(), TypedLobby.Default, null, token);
+                               Debug.Log("Room Join OK");
+                           }
+                           catch (Exception e)
+                           {
+                               startButton.interactable = true;
+                               Debug.LogError(e.Message);
+                           }
+                       }).AddTo(gameObject);
+        }
     }
 }
