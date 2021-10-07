c67e4a7863113f6eb5bdf1ad577e4740fc38c14e
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-07 23:06:25 +0900

    タイトルシーケンスComponentをとりあえず定義

---
+++ Assets/Scripts/Sequence/TitleSequence.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sequence
+{
+    /// <summary>
+    /// タイトルシーケンス
+    /// </summary>
+    public class TitleSequence : MonoBehaviour
+    {
+    }
+}
