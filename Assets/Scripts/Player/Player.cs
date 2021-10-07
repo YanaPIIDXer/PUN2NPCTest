7c36017d88be90f733c9cdf6c905b3358201750f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-07 23:39:43 +0900

    プレイヤークラスをとりあえず定義 PhotonViewをRequireComponentしているので無事にInstantiateできるようになった

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+namespace Player
+{
+    /// <summary>
+    /// プレイヤークラス
+    /// </summary>
+    [RequireComponent(typeof(PhotonView))]
+    public class Player : MonoBehaviour
+    {
+    }
+}
