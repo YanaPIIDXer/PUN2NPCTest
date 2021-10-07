0f804c618ce1384135026578d647fdf2eb0e7e41
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-07 23:43:25 +0900

    移動処理をサクッと実装

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -11,5 +11,30 @@ namespace Player
     [RequireComponent(typeof(PhotonView))]
     public class Player : MonoBehaviour
     {
+        /// <summary>
+        /// 移動ベクトル
+        /// </summary>
+        private Vector2 moveVec = Vector2.zero;
+
+        /// <summary>
+        /// RigidBody
+        /// </summary>
+        private Rigidbody rigidBody = null;
+
+        void Awake()
+        {
+            rigidBody = GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            moveVec.x = Input.GetAxisRaw("Horizontal");
+            moveVec.y = Input.GetAxisRaw("Vertical");
+        }
+
+        void FixedUpdate()
+        {
+            rigidBody.velocity = new Vector3(moveVec.x, 0.0f, moveVec.y);
+        }
     }
 }
